a623a66580f5ee3a0585fa35a421758049cd99f2
Blkmagi <ajsinishin@gmail.com>
2024-12-01 11:36:49 +0800

    Pruned test code.

--- Calculator/main.cs
+++ Calculator/main.cs
@@ -7,34 +7,5 @@
       int eval = Lang.evalExpression(Parser.parseExpression("   2    add    3 +4 plus 60 add    5"));
       Console.WriteLine("The result is " + eval);
     }
-    public static Lang.Expression constructSimple(){
-      Lang.Expression expression = new Lang.Expression();
-      Lang.Add b = new Lang.Add();
-      Lang.Num c = new Lang.Num();
-      Lang.Num d = new Lang.Num();
-      c.a = 2;
-      d.a = 2;
-      b.a.a = c;
-      b.b.a = d;
-      expression.a = b;
-      return expression;
-    }
-    public static Lang.Expression constructComplex(){
-      Lang.Expression expression = new Lang.Expression();
-      Lang.Add b = new Lang.Add();
-      Lang.Num c = new Lang.Num();
-      Lang.Add d = new Lang.Add();
-      Lang.Num e = new Lang.Num();
-      Lang.Num f = new Lang.Num();
-      c.a = 2;
-      e.a = 3;
-      f.a = 5;
-      d.a.a = e;
-      d.b.a = f;
-      b.a.a = c;
-      b.b.a = d;
-      expression.a = b;
-      return expression;
-    }
   }
 }
